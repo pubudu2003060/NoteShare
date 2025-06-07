22cf7d070eccaeb04e44805cd8ffac3f9fb35642
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-06-07 14:25:46 +0530

    make connection with mongodb

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const dbUri = process.env.DB_URI;
+if (!dbUri) {
+  throw new Error("DB_URI environment variable is not defined");
+}
+await mongoose.connect(dbUri).then(() => {
+  console.log("✅ Connected to MongoDB");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello TypeScript + ESM + Express!");
